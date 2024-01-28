274e43f131a96f9fdfb7afb95c46250444d20ffe
AmarettiL <luca.amaretti@studenti.unimi.it>
2024-01-29 00:41:54 +0100

    Starting the highlight of text.

--- kilo.c
+++ kilo.c
@@ -42,6 +42,11 @@ enum editorKey {
   PAGE_DOWN
 };
 
+enum editorHighlight {
+  HL_NORMAL = 0,
+  HL_NUMBER
+};
+
 /*** data ***/
 
 // tipo di dato per salvare una riga di testo
@@ -50,6 +55,7 @@ typedef struct erow {
   int rsize;
   char *chars;
   char *render;
+  unsigned char *hl;
 } erow;
 
 struct editorConfig {
@@ -212,6 +218,20 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+/*** syntax highlighting ***/
+
+void editorUpdateSyntax(erow *row) {
+  row->hl = realloc(row->hl, row->rsize);
+  memset(row->hl, HL_NORMAL, row->rsize);
+
+  int i;
+  for (i = 0; i < row->rsize; i++) {
+    if (isdigit(row->render[i])) {
+      row->hl[i] = HL_NUMBER;
+    }
+  }
+}
+
 /*** row operations ***/
 
 int editorRowCxToRx(erow *row, int cx) {
@@ -262,6 +282,8 @@ void editorUpdateRow(erow *row) {
   }
   row->render[idx] = '\0';
   row->rsize = idx;
+
+  editorUpdateSyntax(row);
 }
 
 void editorInsertRow(int at, char *s, size_t len) {
@@ -280,6 +302,7 @@ void editorInsertRow(int at, char *s, size_t len) {
 
   E.row[at].rsize = 0;
   E.row[at].render = NULL;
+  E.row[at].hl = NULL;
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
@@ -289,6 +312,7 @@ void editorInsertRow(int at, char *s, size_t len) {
 void editorFreeRow(erow *row) {
   free(row->render);
   free(row->chars);
+  free(row->hl);
 }
 
 void editorDelRow(int at) {
@@ -587,7 +611,17 @@ void editorDrawRows(struct abuf *ab) {
 	len = 0;
 
       if (len > E.screencols) len = E.screencols;
-      abAppend(ab, &E.row[filerow].render[E.coloff], len);
+      char *c = &E.row[filerow].render[E.coloff];
+      int j;
+      for (j = 0; j < len; j++) {
+	if (isdigit(c[j])) {
+	  abAppend(ab, "\x1b[31m", 5);
+	  abAppend(ab, &c[j], 1);
+	  abAppend(ab, "\x1b[39m", 5);
+	} else {
+	  abAppend(ab, &c[j], 1);
+	}
+      }
     }
 
     abAppend(ab, "\x1b[K", 3);
